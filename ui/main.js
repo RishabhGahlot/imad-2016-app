d8af113379cbc510a4177ee1f9e58d897e6f1fff
RishabhGahlot <rishabhprobb@gmail.com>
2016-10-13 21:49:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+var img = document.getElementById("fotu");
+var marginLeft=0;
+function moveRight(){
+    marginleft = marginleft + 10 ;
+    img.style.marginLeft = marginLeft + 'px' ;
+    
+}
+img.onclick = function(){
+    var interval = setInterval (moveRight,50);
+};
